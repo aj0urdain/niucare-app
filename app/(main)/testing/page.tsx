3166796d4eee2d2b63187c872a18d7676d1101b7
aaronjgirton <agirton@intradark.com>
2025-05-26 10:05:32 +1000

    chore(testing): update testing page

--- app/(main)/testing/page.tsx
+++ app/(main)/testing/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 
-import { TestVerifyClaimQuery } from "@/components/atoms/test-verify-claim";
-import { TestBankDetailsQuery } from "@/components/atoms/test-has-bank";
-import UseReducerTest from "@/components/atoms/use-reducer-test";
-import ZustandTest from "@/components/atoms/zustand-test";
-import BankTest from "@/components/atoms/test-bank-add";
-import { useEffect } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function TestVerifyClaimPage() {
   return (
     <div className="container">
       <h1 className="text-2xl font-bold mb-6">Verify Claim Test</h1>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="bg-white px-4 py-2 border rounded-md shadow-sm hover:bg-gray-50">
+          Select an option
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Option 1</DropdownMenuItem>
+          <DropdownMenuItem>Option 2</DropdownMenuItem>
+          <DropdownMenuItem>Option 3</DropdownMenuItem>
+          <DropdownMenuItem>Option 4</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
       <div className="flex gap-4">
         {/* <TestVerifyClaimQuery /> */}
         {/* <TestBankDetailsQuery /> */}
@@ -19,7 +32,6 @@ export default function TestVerifyClaimPage() {
       <div className="flex gap-4 mx-auto">
         {/* <UseReducerTest /> */}
         {/* <ZustandTest /> */}
-        <p>Darnell Bourne</p>
       </div>
     </div>
   );
