0959d82919605f9b1050c68f96efd550e2c63c01
aaronjgirton <agirton@intradark.com>
2025-06-02 02:35:52 +1000

    docs(utils): add documentation to amplify utilities

--- utils/amplify-utils.ts
+++ utils/amplify-utils.ts
@@ -1,12 +1,37 @@
+/**
+ * @file amplify-utils.ts
+ * @description Utility functions for AWS Amplify server-side operations and authentication
+ * @author Aaron J. Girton - https://github.com/aj0urdain
+ * @created 2025
+ */
+
 import { createServerRunner } from "@aws-amplify/adapter-nextjs";
 import { cookies } from "next/headers";
 import { getCurrentUser } from "aws-amplify/auth/server";
 import { config } from "@/config/aws-config";
 
+/**
+ * Creates a server runner for AWS Amplify operations
+ * This is used to run Amplify operations in a server context
+ * @type {Object}
+ */
 export const { runWithAmplifyServerContext } = createServerRunner({
   config: config as any,
 });
 
+/**
+ * Checks if the current user is authenticated
+ * @returns {Promise<boolean>} True if the user is authenticated, false otherwise
+ *
+ * @example
+ * ```ts
+ * // In a server component or API route
+ * const isUserAuthenticated = await isAuthenticated();
+ * if (!isUserAuthenticated) {
+ *   // Handle unauthenticated state
+ * }
+ * ```
+ */
 export const isAuthenticated = async () =>
   await runWithAmplifyServerContext({
     nextServerContext: { cookies },
